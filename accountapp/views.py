662a0726ffa248515f37744292d24b884bce59e2
dayoorang <dayoorang@gmail.com>
2021-12-09 15:52:40 +0900

    axios post update

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,6 +9,11 @@ from rest_framework.response import Response
 def hello_world_template(request):
     return render(request, 'accountapp/hello_world.html')
 
-@api_view()
+@api_view(['GET','POST'])
 def hello_world(request):
+
+    if request.method == 'POST':
+        input_data = request.data.get('input_data')
+        return Response({'message': input_data})
+
     return Response({'message':'return text'})
